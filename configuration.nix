f20a8d625430346e387a0872745e28f862509350
Christoph <72564159+chrisinick@users.noreply.github.com>
2025-02-12 22:59:00 +0100

    Setup basic configuration

---
+++ configuration.nix
@@ -0,0 +1,173 @@
+{ config, lib, pkgs, inputs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+    ];
+
+  # systemd-boot EFI boot loader
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 4;
+    };
+    efi.canTouchEfiVariables = true;
+    timeout = 4;
+  };
+
+  # Network
+  networking.hostName = "chris-desktop";
+  systemd.network = {
+    enable = true;
+    networks."10-wired" = {
+      matchConfig.Name = "en*";
+      networkConfig.DHCP = "yes";
+    };
+  };
+  services.resolved.enable = true;
+
+  # Time zone
+  time.timeZone = "Europe/Berlin";
+
+  # Internationalisation
+  i18n.defaultLocale = "de_DE.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "de";
+  };
+
+  # Hardware settings
+  hardware.graphics.enable = true;
+
+  # X11, Wayland, Gnome
+  services.xserver = {
+    enable = true;
+    desktopManager.gnome.enable = true;
+    displayManager.gdm.enable = true;
+
+    # Keymap in X11 and Wayland
+    xkb.layout = "de";
+    xkb.options = "caps:escape";
+
+  };
+  services.displayManager.defaultSession = "gnome";
+  programs.xwayland.enable = true;
+
+  systemd.user.services.gnomeSettingsDaemon.enable = true;
+  systemd.user.services.gnomeKeyring.enable = true;
+
+  # Sound
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # CUPS for printing
+  # services.printing.enable = true;
+
+  # Touchpad support (enabled default in most desktopManager)
+  # services.libinput.enable = true;
+
+  # User account (don't forget to set a password with ‘passwd’)
+  users.users.chris = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" "audio" ];
+  };
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      chris = import ./home.nix;
+    };
+  };
+
+  # Session variables
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # Nix packages configuration
+  nixpkgs.config.allowUnfree = true;
+
+  # Enabled programs
+  programs.firefox.enable = true;
+
+  # Packages installed in system profile
+  environment.systemPackages = with pkgs; [
+    wget
+    vim
+  ];
+
+  # Excluded packages
+  environment.gnome.excludePackages = with pkgs; [
+    cheese
+    eog
+    epiphany
+    evince
+    geary
+    gnome-maps
+    gnome-music
+    gnome-weather
+    gedit
+    gnome-connections
+    gnome-photos
+    gnome-text-editor
+    gnome-tour
+    snapshot
+    simple-scan
+    totem
+    yelp
+  ];
+
+  services.xserver.excludePackages = with pkgs; [
+    xterm
+  ];
+
+  # Automatic updates
+  # system.autoUpgrade = {
+    # enable = true;
+    # flake = "github:nixos/nixpkgs?ref=nixos-TODO";
+    # flags = [
+      # "--update-input"
+      # "nixpkgs"
+      # "--commit-lock-file"
+    # ];
+    # dates = "daily";
+    # randomizedDelaySec = "45min";
+  # };
+
+  # Nix settings
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Maintenance automation
+  nix = {
+    settings.auto-optimise-store = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 15d";
+    };
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  system.stateVersion = "24.11";
+
+}
+
---
+++ hardware-configuration.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/nixos";
+      fsType = "f2fs";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/BOOT";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ ];
+
+  networking.useDHCP = lib.mkDefault false;
+  networking.interfaces.enp7s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
