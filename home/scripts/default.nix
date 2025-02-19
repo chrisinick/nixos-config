60a1c130f6253429c323c6a11db31303fd5b7ce7
Christoph <72564159+chrisinick@users.noreply.github.com>
2025-02-19 19:30:51 +0100

    173 current  1970-01-01 01:00:00  25.05.20250213.2ff53fe  6.12.13                          *

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -5,6 +5,10 @@
   ...
 }:
 {
+  home.sessionPath = [
+    "$HOME/scripts"
+  ];
+
   home.file = {
     # Sort Gnome app grid
     ".config/autostart/sort-app-grid.desktop" = {
@@ -45,6 +49,28 @@
             notify-send "Error" "Selected file is not a valid .xz or .tar.xz archive."
             exit 1
         fi
+      '';
+      executable = true;
+    };
+
+    # University VPN
+    "scripts/th-nuernberg_vpn.sh" = {
+      text = ''
+        #!/usr/bin/env bash
+        MTU=1284
+        HIREPORT=${pkgs.openconnect}/libexec/openconnect/hipreport.sh
+        DEADPEERSSECS=30
+        IFACE=vpn0
+        set +o histexpand
+        sudo ${pkgs.openconnect}/bin/openconnect \
+          --protocol gp \
+          --syslog \
+          --disable-ipv6 \
+          --csd-wrapper $HIREPORT \
+          --interface $IFACE \
+          --mtu $MTU \
+          --force-dpd $DEADPEERSSECS \
+          vpn.ohmportal.de
       '';
       executable = true;
     };
