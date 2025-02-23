2bb0947e9b5e5ad60fcb66346a0f77285174f0dd
Christoph <72564159+chrisinick@users.noreply.github.com>
2025-02-23 13:18:35 +0100

    183 current  1970-01-01 01:00:00  25.05.20250218.73cf49b  6.12.14                          *

--- modules/system.nix
+++ modules/system.nix
@@ -62,7 +62,11 @@
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    jack.enable = true;
   };
+  security.rtkit.enable = true;
 
   # CUPS for printing
   # services.printing.enable = true;
