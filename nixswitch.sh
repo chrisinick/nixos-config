20cc5a6597f5181c893bc5031b55d6cb17c02686
Christoph <72564159+chrisinick@users.noreply.github.com>
2025-04-07 10:04:10 +0200

    242 current  1970-01-01 01:00:00  25.05.20250405.42a1c96  6.12.21                          *

---
+++ nixswitch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+trap "git reset; exit 1" ERR
+find . -name "*.nix" -exec nixfmt {} \;
+git diff -U0 *.nix
+echo "NixOS rebuilding..."
+git add .
+sudo nixos-rebuild switch --impure --flake .
+gen=$(nixos-rebuild list-generations | grep current)
+git commit -am "$gen"
