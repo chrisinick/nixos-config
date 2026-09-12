95b85c39fe742d5fcb420806e4dcfc5b6f2237a2
Christoph <72564159+chrisinick@users.noreply.github.com>
2026-09-12 18:58:56 +0200

    NixOS 26.11.20260910.8ce4ef6 | kernel 7.2.4 | 2026-09-12 18:58:53

--- system/programs.nix
+++ system/programs.nix
@@ -45,6 +45,9 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
-    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+    extraCompatPackages = with pkgs; [
+      adwaita-icon-theme
+      proton-ge-bin
+    ];
   };
 }
