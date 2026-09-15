5ba4292422bf13c76ed2c92092abd229ec78635b
Christoph <72564159+chrisinick@users.noreply.github.com>
2026-09-15 12:01:17 +0200

    NixOS 26.11.20260910.8ce4ef6 | kernel 7.2.4 | 2026-09-15 10:46:59

--- system/default.nix
+++ system/default.nix
@@ -100,13 +100,6 @@
   systemd.user.services.gnomeKeyring.enable = true;
   services.gnome.games.enable = false;
 
-  # Qt
-  qt = {
-    enable = true;
-    style = "adwaita-dark";
-    platformTheme = "gnome";
-  };
-
   # X Keymap
   services.xserver.xkb = {
     layout = "de";
